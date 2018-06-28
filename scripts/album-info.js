294106f101f9a53dbeb4144f296617c90fa5d667
Tristine Nguyen <tristine@mail.usf.edu>
2018-06-28 03:04:51 -0400

    Assignment 8 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -2,3 +2,11 @@
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
 }
+
+{
+  $('.artist').text(album.artist);
+}
+
+{
+  $('#release-info').text(album.releaseInfo);
+}
